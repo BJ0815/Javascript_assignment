cc9a3b9a242dec5bf8379e8ede7c3187cc6b551a
BJ0815 <jtt11220@gmail.com>
2018-01-17 16:35:28 +0800

    complete js assignment 1

---
+++ assignment-1/js/javascript.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var state = "on";
+function toggleLight() {
+  var image = document.getElementById("image");
+  state = !state;
+  image.src = state ? "img/on.jpg" : "img/off.jpg";
+}
+
+var timeLeft = "";
+function reloading(){
+  timeLeft = document.getElementById("input-time").value;
+  if (timeLeft == ""){
+    return alert("請輸入數值");
+  }
+  document.getElementById("input-time").value = "";
+  id = setInterval(countDownTimer, 1000);
+}
+var id = "";
+
+function countDownTimer() {
+  if (timeLeft <= 0) {
+    document.getElementById("image").src = "img/off.jpg";
+    clearInterval(id);
+  }
+  document.getElementById("timer").innerHTML = timeLeft;
+  timeLeft--;
+}
+
